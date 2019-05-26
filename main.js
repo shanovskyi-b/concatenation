f01e3ef7d3ef7d6c8c9170115c59e0ced3e09ba0
shanovskyi-b <shanovskyi.b@gmail.com>
2019-05-26 19:59:52 +0300

    added images adding and removing

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 class ImageField {
-  constructor ({onImageLoad}) {
-    this.onImageLoad = onImageLoad;
+  constructor ({onLoad}) {
+    this.onLoad = onLoad;
     this.image = null;
     this.loaded = false;
 
@@ -26,17 +26,19 @@ class ImageField {
     this.image = new Image();
     this.image.onload = () => {
       this.loaded = true;
-      this.onImageLoad();
+      this.onLoad();
     }
     this.image.src = url;
   }
 }
 
 class ImagesList {
-  constructor({onListChange, container}) {
+  constructor({container, onChange}) {
     this.list = [];
     this.element = container;
-    this.onListChange = onListChange;
+    this.onChange = onChange;
+
+    this.initAddImageButton();
 
     this.addImage();
     this.addImage();
@@ -45,28 +47,66 @@ class ImagesList {
   addImage() {
     let image = {
       field: new ImageField({
-        onImageLoad: () => this.onListChange(this.getLoadedImages()),
+        onLoad: () => this.onChange(this.getLoadedImages()),
       }),
-      container: document.createElement('div')
+      container: document.createElement('div'),
+      removeButton: document.createElement('button')
     };
 
+    image.container.classList.add('image-container');
+
+    image.removeButton.innerHTML = 'Remove';
+    image.removeButton.classList.add('remove-image');
+    image.removeButton.addEventListener(
+      'click',
+      () => this.removeImage(image)
+    );
+
     image.container.appendChild(image.field.element);
+    image.container.appendChild(image.removeButton);
 
     this.list.push(image);
-    this.element.appendChild(image.container);
+
+    this.element.insertBefore(image.container, this.addImageButton);
   }
 
   getLoadedImages() {
+    console.log('here', this.list
+      .filter((element) => element.field.loaded)
+      .map((element) => element.field.image)
+    )
     return this.list
       .filter((element) => element.field.loaded)
       .map((element) => element.field.image);
+  }
+
+  initAddImageButton() {
+    this.addImageButton = document.createElement('button');
+    this.addImageButton.innerHTML = 'Add Image';
+    this.addImageButton.classList.add('add-image');
+
+    this.addImageButton.addEventListener(
+      'click',
+      () => this.addImage()
+    );
+
+    this.element.appendChild(this.addImageButton);
+  }
+
+  removeImage(image) {
+    let index = this.list.indexOf(image);
+    if (index !== -1) {
+      this.list.splice(index, 1);
+      image.container.remove();
+      this.onChange(this.getLoadedImages());
+    }
   }
 }
 
 class ConcatenationCanvas {
   constructor (element) {
     this.canvas = element;
-    console.log('canvas is', this.canvas.offsetHeight)
+
     this.canvas.height = this.canvas.offsetHeight;
     this.canvas.width = this.canvas.offsetWidth;
 
@@ -164,7 +204,7 @@ function init () {
 
   let images = new ImagesList({
     container: imagesContainer,
-    onListChange: canvas.drawImages.bind(canvas)
+    onChange: (images) => canvas.drawImages(images)
   });
 }
 
